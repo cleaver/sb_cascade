d3815014126343516999ec67338f80251f3cace6
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2024-09-18 17:42:09 -0400

    add more aliases to .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,6 +1,8 @@
 import Ecto.Query
+alias SbCascade.Accounts
 alias SbCascade.Accounts.{User, UserToken}
 alias SbCascade.Content
 alias SbCascade.Content.{Comic, Tag, Page}
 alias SbCascade.Repo
+alias SbCascade.Site
 alias SbCascade.Site.Setting
